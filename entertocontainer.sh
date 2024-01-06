19e1f2b246d6ff06f935fbe9cc5fba67cf69e6b9
Halhadus <76776068+Halhadus@users.noreply.github.com>
2024-01-06 23:00:51 +0300

    i created script for myself, i'm too lazy to create a universal script
    Ski-bi-dibby-dib yo-da-dub-dub, yo-da-dub-dub

---
+++ entertocontainer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Don't forget install linux-packaging-snippets
+export KERNEL_DIR="$HOME/droidian/kernel/xiaomi/begonia"
+export PACKAGES_DIR="$HOME/droidian/packages"
+mkdir -p $PACKAGES_DIR
+sudo docker run --rm -v $PACKAGES_DIR:/buildd -v $KERNEL_DIR:/buildd/sources -it quay.io/droidian/build-essential:bookworm-amd64 bash

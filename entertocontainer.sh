791968a98e2831db647ef7d9c0d1afd85a4a5289
Halhadus <76776068+Halhadus@users.noreply.github.com>
2024-01-07 01:46:31 +0300

    uodated container
    Lemide heyranin olim

--- entertocontainer.sh
+++ entertocontainer.sh
@@ -3,4 +3,4 @@
 export KERNEL_DIR="$HOME/droidian/kernel/xiaomi/begonia"
 export PACKAGES_DIR="$HOME/droidian/packages"
 mkdir -p $PACKAGES_DIR
-sudo docker run --rm -v $PACKAGES_DIR:/buildd -v $KERNEL_DIR:/buildd/sources -it quay.io/droidian/build-essential:bookworm-amd64 bash
+sudo docker run --rm -v $PACKAGES_DIR:/buildd -v $KERNEL_DIR:/buildd/sources -it quay.io/droidian/build-essential:trixie-amd64 bash
